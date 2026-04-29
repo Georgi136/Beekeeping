d891c24b26de8d652c6184efc9983a69a351cfda
Georgi136 <georgisakaliyski@gmail.com>
2026-04-29 23:12:17 +0300

    Google anl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
+import { useEffect } from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Products from './components/Products'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import ReactGA from 'react-ga4'
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search })
+  }, [])
+
   return (
     <>
       <Header />
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { LanguageProvider } from './i18n/LanguageContext'
 import './index.css'
 import App from './App.tsx'
+import ReactGA from 'react-ga4'
+
+// Initialize Google Analytics
+ReactGA.initialize('G-D9FP0ZFK1S')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
